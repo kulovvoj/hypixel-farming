300d38972ab763e3424e180fb0f9793e2d969310
kulovvoj <vojtech.kulovany@seznam.cz>
2024-09-07 08:21:03 +0200

    gh-pages setup + removed i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './localization/i18n.ts'
 import './index.scss'
 import 'bootstrap/dist/css/bootstrap.css'
 
