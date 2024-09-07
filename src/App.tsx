293be6497a0fe7a9afa2a7f33d189802cbdf68b5
kulovvoj <vojtech.kulovany@seznam.cz>
2024-09-07 08:28:42 +0200

    fixups

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <Router basename='/'>
+    <Router basename='/hypixel-farming'>
       <AppRouter />
     </Router>
   )
