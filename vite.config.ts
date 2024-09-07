d6cd1179462bbcb556bf17e5d7e5a24ad37f9c9a
kulovvoj <vojtech.kulovany@seznam.cz>
2024-09-07 08:35:30 +0200

    base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/hypixel-farming',
   plugins: [
     svgr({
       // svgr options: https://react-svgr.com/docs/options/
